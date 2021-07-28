ad400f4aff967d89cb5628f74dd62e912ce37082
2priyageorge <69159549+2priyageorge@users.noreply.github.com>
2021-07-28 13:58:55 -0500

    mini project 1: personalized app

---
+++ KWK Folder/miniProjectPersonalizedApp/miniProjectPersonalizedApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  miniProjectPersonalizedApp
+//
+//  Created by Scholar on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    }
+
+    @IBAction func learnMore(_ sender: Any) {
+    }
+    
+}
+
+
